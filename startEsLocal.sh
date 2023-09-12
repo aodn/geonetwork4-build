c6a7b6e199e5968e39e3dc0005d80cc7e998aa0a
utas-raymondng <manfai.ng@utas.edu.au>
2023-09-12 11:29:11 +1000

    Add back ssl

--- startEsLocal.sh
+++ startEsLocal.sh
@@ -3,13 +3,16 @@
 export UID=$(id -u)
 export GID=$(id -g)
 
-# If you see permission error on creating the cert in log, then there is a problem with the ip where
-# the program have no permission to access it.
+# If you see permission error on creating the cert in log, then there is a problem with the elasticdata,
+# the elasticdata folder and it subdirectory needs to have $UID:$GID
 
 # Create the folder if it doesn't exist
 if [ ! -d "elasticdata" ]; then
     mkdir -p "elasticdata"
     chown $UID:$GID "elasticdata"
+
+    mkdir -p "elasticdata/certs"
+    chown $UID:$GID "elasticdata/certs"
 
     mkdir -p "elasticdata/elasticsearch-data"
     chown $UID:$GID "elasticdata/elasticsearch-data"
@@ -19,6 +22,8 @@ if [ ! -d "elasticdata" ]; then
 
     mkdir -p "elasticdata/enterprisesearch-data"
     chown $UID:$GID "elasticdata/enterprisesearch-data"
+else
+    chown -R $UID:$GID "elasticdata"
 fi
 
 docker-compose -f docker-es-compose.yml up
