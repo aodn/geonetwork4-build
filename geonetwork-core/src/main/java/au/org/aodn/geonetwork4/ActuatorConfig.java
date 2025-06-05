3d6040204c2cf4dfcf939671e4a8d4bee799bec0
rng <manfai.ng@utas.edu.au>
2025-06-05 14:29:36 +1000

    First working check point

--- geonetwork-core/src/main/java/au/org/aodn/geonetwork4/ActuatorConfig.java
+++ geonetwork-core/src/main/java/au/org/aodn/geonetwork4/ActuatorConfig.java
@@ -1,6 +1,9 @@
 package au.org.aodn.geonetwork4;
 
 import au.org.aodn.geonetwork_api.openapi.api.helper.SiteHelper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -24,7 +28,8 @@ public class ActuatorConfig {
     public HealthIndicator geonetworkHealthIndicator(
             org.springframework.core.env.Environment environment,
             RestTemplate template,
-            Setup setup) {
+            Setup setup,
+            ObjectMapper objectMapper) {
 
         return () -> {
             String[] profiles = environment.getActiveProfiles();
@@ -47,37 +52,53 @@ public class ActuatorConfig {
                 protocol = setup.getSiteSetting(SiteHelper.PROTOCOL);
             }
 
-            ResponseEntity<List<Map<String, String>>> response = template.exchange(
-                    String.format(CRITICAL_HEALTHCHECK, protocol, host, port),
-                    HttpMethod.GET,
-                    null,
-                    new ParameterizedTypeReference<>() {});
+            try {
+                ResponseEntity<List<Map<String, String>>> response = template.exchange(
+                        String.format(CRITICAL_HEALTHCHECK, protocol, host, port),
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<>() {
+                        });
 
-            if(response.getStatusCode().is2xxSuccessful()) {
-                if (response.getBody() != null) {
-                    // We need to make sure all critical component is health in order to report healthy
-                    Optional<Map<String, String>> target = response.getBody()
-                            .stream()
-                            .filter(p -> !(p.containsKey("status") && p.get("status").equalsIgnoreCase("OK")))
-                            .findFirst();
-
-                    return target.<HealthIndicator>map(
-                            v -> Health.outOfService()
-                                    .withDetails(Map.of(
-                                            "info", "GeoNetwork4 missing critical service",
-                                            "service", v.get("name"),
-                                            "msg", v.get("msg")
-                                    ))::build
-                            )
-                            .orElseGet(() -> Health.up()
-                                    .withDetail("info", "GeoNetwork4")
-                                    ::build
-                            ).health();
+                if (response.getStatusCode().is2xxSuccessful()) {
+                    if (response.getBody() != null) {
+                        return createHealthResponse(response.getBody());
+                    }
+                }
+            }
+            catch(HttpServerErrorException.InternalServerError e) {
+                try {
+                    List<Map<String, String>> value = objectMapper.readValue(e.getResponseBodyAsString(), new TypeReference<>() {});
+                    return createHealthResponse(value);
+                }
+                catch (JsonProcessingException ex) {
+                    // Do nothing
                 }
             }
             return Health.down()
-                    .withDetail("info", "GeoNetwork4 critical health check status not OK or missing body content")
+                    .withDetail("info", "GeoNetwork4 critical health check status not OK or malform body content")
                     .build();
         };
+    }
+
+    protected Health createHealthResponse(List<Map<String, String>> value) {
+        // We need to make sure all critical component is health in order to report healthy
+        Optional<Map<String, String>> target = value
+                                .stream()
+                .filter(p -> !(p.containsKey("status") && p.get("status").equalsIgnoreCase("OK")))
+                .findFirst();
+
+        return target.<HealthIndicator>map(
+                        v -> Health.outOfService()
+                                .withDetails(Map.of(
+                                        "info", "GeoNetwork4 missing critical service",
+                                        "service", v.get("name"),
+                                        "msg", v.get("msg")
+                                ))::build
+                )
+                .orElseGet(() -> Health.up()
+                        .withDetail("info", "GeoNetwork4")
+                        ::build
+                ).health();
     }
 }
