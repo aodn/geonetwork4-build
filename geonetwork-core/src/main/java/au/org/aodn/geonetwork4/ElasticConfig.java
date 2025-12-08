29d57b827021f092316348a31271dd8baa6cc779
rng <manfai.ng@utas.edu.au>
2025-12-08 14:00:40 +1100

    Fix hard code value

--- geonetwork-core/src/main/java/au/org/aodn/geonetwork4/ElasticConfig.java
+++ geonetwork-core/src/main/java/au/org/aodn/geonetwork4/ElasticConfig.java
@@ -18,11 +18,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Field;
 
 @Configuration
 public class ElasticConfig {
@@ -32,8 +28,8 @@ public class ElasticConfig {
     @Value("${aodn.geonetwork4.elasticFieldLimit:5000}")
     protected Long elasticFieldLimit;
 
-    @Value("${aodn.geonetwork4.elasticRecordLimit:4.5e+8}")
-    protected Long elasticRecordLimit;
+    @Value("${aodn.geonetwork4.commitInterval:100}")
+    protected int commitInterval;
 
     @Value("${es.index.records}")
     protected String recordIndexName;
@@ -48,6 +44,19 @@ public class ElasticConfig {
      */
     @PostConstruct
     public void init() {
+        JeevesApplicationContext ctx = (JeevesApplicationContext)ApplicationContextHolder.get();
+        EsSearchManager esSearchManagerRawBean = ctx.getBean(EsSearchManager.class);
+        // Override the commitInterval value which is hardcode in the geonetwork and no way to change with
+        // param, this value control how many docs store in memory before batch write to elastic, unfortunately
+        // number of docs is not reliable and may result in total docs size too big for bulkSave. Here we reduce
+        // it to 100 from 200 and pray it works.
+        try {
+            logger.info("Override document commit interval to {}", commitInterval);
+            Field field = esSearchManagerRawBean.getClass().getDeclaredField("commitInterval");
+            field.setAccessible(true);
+            field.setInt(esSearchManagerRawBean, commitInterval);
+        } catch (Exception ignored) {}
+
         class EsSearchManagerInterceptor implements MethodInterceptor {
             @Nullable
             @Override
@@ -76,81 +85,16 @@ public class ElasticConfig {
             }
         }
 
-        JeevesApplicationContext ctx = (JeevesApplicationContext)ApplicationContextHolder.get();
         ctx.setAllowBeanDefinitionOverriding(true);
-
-        EsSearchManager esSearchManagerRawBean = ctx.getBean(EsSearchManager.class);
-        EsRestClient esRestClientRawBean = ctx.getBean(EsRestClient.class);
-
-        class EsRestClientInterceptor implements MethodInterceptor {
-            @Nullable
-            @Override
-            public Object invoke(@Nonnull MethodInvocation invocation) throws Throwable {
-                if ("bulkRequest".equals(invocation.getMethod().getName()) &&
-                        invocation.getArguments().length == 2 &&
-                        invocation.getArguments()[0] instanceof String &&
-                        invocation.getArguments()[1] instanceof Map) {
-                    // Geonetwork do not limit the document size and therefore sometimes the bulk save failed due to too
-                    // large of bulk size.
-                    String index = invocation.getArguments()[0].toString();
-
-                    @SuppressWarnings("unchecked")
-                    Map<String, String> documents = (Map<String, String>)invocation.getArguments()[1];
-
-                    // Make sure the number of doc is not too large
-                    for(Map<String, String> doc : splitBySize(documents)) {
-                        if(!doc.isEmpty()) {
-                            esRestClientRawBean.bulkRequest(index, doc);
-                        }
-                    }
-                }
-                return invocation.proceed();
-            }
-            /**
-             * Split the map by inspecting the size entry value, if greater than the set value, we split the map
-             * @param data - Original unsplit value.
-             * @return Possible split map
-             */
-            protected List<Map<String, String>> splitBySize(Map<String, String> data) {
-                long byteCount = 0;
-                Map<String, String> map = new HashMap<>();
-
-                List<Map<String, String>> split = new ArrayList<>();
-                split.add(map);
-
-                for(Map.Entry<String, String> entry : data.entrySet()) {
-                    map.put(entry.getKey(), entry.getValue());
-                    byteCount += entry.getValue().getBytes(StandardCharsets.UTF_8).length;
-
-                    if (byteCount >= elasticRecordLimit) {
-                        logger.info("Batch larger than {} bytes, split", elasticRecordLimit);
-                        // If it is greater than 400mb
-                        byteCount = 0;
-                        map = new HashMap<>();
-                        split.add(map);
-                    }
-                }
-                return split;
-            }
-        }
 
         ProxyFactory factory1 = new ProxyFactory(esSearchManagerRawBean);
         factory1.setProxyTargetClass(true); // CGLIB for XML beans
         factory1.addAdvice(new EsSearchManagerInterceptor());
         EsSearchManager proxiedBean1 = (EsSearchManager) factory1.getProxy();
 
-        ProxyFactory factory2 = new ProxyFactory(esRestClientRawBean);
-        factory2.setProxyTargetClass(true); // CGLIB for XML beans
-        factory2.addAdvice(new EsRestClientInterceptor());
-        EsRestClient proxiedBean2 = (EsRestClient) factory2.getProxy();
-
         DefaultListableBeanFactory f = (DefaultListableBeanFactory)ctx.getBeanFactory();
         String[] names = ctx.getBeanNamesForType(EsSearchManager.class);
         f.destroySingleton(names[0]);
         f.registerSingleton(names[0], proxiedBean1); // Add proxied
-
-        names = ctx.getBeanNamesForType(EsRestClient.class);
-        f.destroySingleton(names[0]);
-        f.registerSingleton(names[0], proxiedBean2); // Add proxied
     }
 }
